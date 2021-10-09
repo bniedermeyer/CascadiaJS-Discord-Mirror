baf22ff7c06d3fa72963aba99f367dcebe9ff21a
Brenden Niedermeyer <contact@brenden.dev>
2021-10-09 15:34:24 +0200

    feat: add auto scrolling and remove container styling
    Adds functionality to allow the latest message to always be visible to a user, but not force them to
    scroll if they are scrolling earlier in chat history. Also removes styling from the host element so
    that consuming applications can decide to style on their own.

--- src/utils/firebase.service.ts
+++ src/utils/firebase.service.ts
@@ -1,7 +1,6 @@
 import { initializeApp, FirebaseOptions, FirebaseApp } from 'firebase/app';
-import {getAuth, signInAnonymously} from 'firebase/auth'
+import { getAuth, signInAnonymously } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
-
 
 export class FirebaseService {
   private config: FirebaseOptions;
@@ -10,12 +9,13 @@ export class FirebaseService {
   constructor(databaseURL: string, apiKey: string, enableAuth: boolean = true) {
     this.config = { databaseURL };
     if (enableAuth && apiKey) {
-      this.config = {...this.config, apiKey}
+      this.config = { ...this.config, apiKey };
     }
     this.app = initializeApp(this.config);
     if (enableAuth) {
+      // This allows us to put read protection on the database without requiring individual users to authenticate.
       const auth = getAuth(this.app);
-      signInAnonymously(auth).then(() => console.log('🚀'))
+      signInAnonymously(auth).then(() => console.log(`🌲 Hi from CascadiaJS 2021! 🌲`));
     }
   }
 
